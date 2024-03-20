cb42a9f17c560328360e94f5d0f30118e7fad508
Alexanderm2445 <144353603+Alexanderm2445@users.noreply.github.com>
2024-03-20 14:14:29 +0000

    continuing javascript coding

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const cookie = document.querySelector('#gameCookie');
 score = 0;
+scoreIncreaser = 1;
 
 if(cookie){
     cookie.addEventListener("click", addScore);
 }
 
 function addScore(){
-    console.log("hi");
+    const changeText = document.querySelector("#cookieCount");
+
+changeText.addEventListener("click", function()) {
+  changeText.textContent = score;
+  score = score + scoreIncreaser;
+}
+    cookieCount = score;
 }
