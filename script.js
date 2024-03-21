a5d82e81d8bf6869092a9fb02ccf14e283b73110
Alexanderm2445 <144353603+Alexanderm2445@users.noreply.github.com>
2024-03-21 14:14:32 +0000

    continual work on the JavaScript of the cookieClicker

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const cookie = document.querySelector('#gameCookie');
-score = 0;
+const powerUp = document.querySelector('#multiplierButton');
+const multiplierButtonPrice = document.querySelector('#multiplierButtonPrice');
+cookieCount = 0;
 scoreIncreaser = 1;
+multiplierPrice = 50;
 
 if(cookie){
     cookie.addEventListener("click", addScore);
 }
 
-function addScore(){
-    const changeText = document.querySelector("#cookieCount");
-
-changeText.addEventListener("click", function()) {
-  changeText.textContent = score;
-  score = score + scoreIncreaser;
+function addScore() {
+    cookieCount += scoreIncreaser;
+    const newScore = document.getElementById("cookieCount");
+    newScore.innerHTML = `${cookieCount}`;
 }
-    cookieCount = score;
+if(powerUp){
+    powerUp.addEventListener("click", multiplierPowerUp);
+}
+
+function multiplierPowerUp() {
+    if ((cookieCount >= multiplierPrice)) {
+        cookieCount = cookieCount - multiplierPrice;
+        scoreIncreaser = scoreIncreaser * 2;
+        return cookieCount;
+    }
+    multiplierPrice = multiplierPrice * 2;
+    multiplierButtonPrice.innerHTML = "The price is " + `${multiplierPrice}`;
+    
 }
