5905c24c7f364c05126bdc5f86f41c5229f3a83e
Alexanderm2445 <144353603+Alexanderm2445@users.noreply.github.com>
2024-04-11 14:08:18 +0000

    working on js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const cookie = document.querySelector('#gameCookie');
 const powerUp = document.querySelector('#multiplierButton');
+const autoUp = document.querySelector('#autoCookieButton');
 const multiplierButtonPrice = document.querySelector('#multiplierButtonPrice');
+const winnerWinner = document.getElementById('youWinButton');
 cookieCount = 0;
 scoreIncreaser = 1;
 multiplierPrice = 50;
+autoPrice = 100;
+winPrice = 10000;
 
 if(cookie){
     cookie.addEventListener("click", addScore);
@@ -14,6 +18,7 @@ function addScore() {
     const newScore = document.getElementById("cookieCount");
     newScore.innerHTML = `${cookieCount}`;
 }
+
 if(powerUp){
     powerUp.addEventListener("click", multiplierPowerUp);
 }
@@ -22,9 +27,28 @@ function multiplierPowerUp() {
     if ((cookieCount >= multiplierPrice)) {
         cookieCount = cookieCount - multiplierPrice;
         scoreIncreaser = scoreIncreaser * 2;
+        multiplierPrice = multiplierPrice * 2;
+        multiplierButtonPrice.innerHTML = `Price: ${multiplierPrice}`;
         return cookieCount;
     }
-    multiplierPrice = multiplierPrice * 2;
-    multiplierButtonPrice.innerHTML = "The price is " + `${multiplierPrice}`;
-    
+}
+
+if(autoUp){
+    autoUp.addEventListener("click", autoPowerUp);
+}
+
+function autoPowerUp() {
+    if (cookieCount >= autoPrice) {
+        cookieCount = cookieCount - autoPrice;
+        autoPrice = autoPrice * 3;
+        autoCookieButtonPrice.innerHTML = `Price: ${autoPrice}`;
+        return cookieCount;
+    }
+}
+
+function crownTransfer() {
+    if ((cookieCount >= winPrice)) {
+        winnerWinner.addEventListener('click', function()) {
+    }
+}
 }
